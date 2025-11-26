d8a34510fbd8efe8f08bd4aee9df7a3e5bd91024
tarantulass <jeetgovind206@gmail.com>
2025-11-26 13:14:59 +0530

    Completed testing on the boltzmann_trial for movie review dataset

--- config.py
+++ config.py
@@ -2,3 +2,8 @@ import os
 
 ADABOOST_DIR = os.path.join("results", "adaboost")
 RANDOMFOREST_DIR = os.path.join("results", "randomforest")
+RBM_DIR = os.path.join("results", "rbm")
+AUTOENCODER_DIR = os.path.join("results", "autoencoder")
+VAR_DIR = os.path.join("results", "variational_autoencoder")
+
+RAG_DIR = os.path.join("results", "rag")

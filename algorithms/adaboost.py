4f94f54c8a0e2e3ae91df3bafabfcf75d711b4a9
tarantulass <jeetgovind206@gmail.com>
2025-11-26 07:28:48 +0530

    Added the automation for ansys in there

--- algorithms/adaboost.py
+++ algorithms/adaboost.py
@@ -1,2 +1,23 @@
+#### system import
+import os,sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from sklearn.ensemble import AdaBoostRegressor
+from sklearn.model_selection import train_test_split
+from pathlib import Path
+import pandas as pd
+import matplotlib.pyplot as plt
 
+#### local import
+from utils.logmodule import logsetup
+
+def adaboost_regressor(data: Path, n_estimators=50, learning_rate=1.0):
+    logger = logsetup()
+    logger.info("Starting AdaBoost Regressor training...")
+
+    #### loading the dataset
+
+    model = AdaBoostRegressor(n_estimators=n_estimators, learning_rate=learning_rate)
+    model.fit(X_train, y_train)
+    logger.info("AdaBoost Regressor training completed.")
+    predictions = model.predict(X_test)
+    return predictions
--- utils/logmodule.py
+++ utils/logmodule.py
@@ -1,1 +1,10 @@
+import logging
 
+def logsetup():
+    logger = logging.getLogger(f'{__name__}')
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+    return logger
